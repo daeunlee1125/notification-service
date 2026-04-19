22149400e82a2653cf25eb2cd66c9180d5a56e3a
daeunlee1125 <daeunlee1125@gmail.com>
2026-04-19 16:17:07 +0900

    notification insert

--- src/main/java/kr/co/daeun/notification/mapper/NotificationMapper.java
+++ src/main/java/kr/co/daeun/notification/mapper/NotificationMapper.java
@@ -2,7 +2,9 @@ package kr.co.daeun.notification.mapper;
 
 import kr.co.daeun.notification.dto.CreateNotificationReqDTO;
 import kr.co.daeun.notification.dto.NotificationDTO;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface NotificationMapper {
