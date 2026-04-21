0e296d4ad5c18b0a6e42dca91dfe28ada111df90
daeunlee1125 <daeunlee1125@gmail.com>
2026-04-21 20:29:27 +0900

    worker 관ë처리용 매퍼 추가

--- src/main/java/kr/co/daeun/notification/mapper/NotificationMapper.java
+++ src/main/java/kr/co/daeun/notification/mapper/NotificationMapper.java
@@ -1,13 +1,29 @@
 package kr.co.daeun.notification.mapper;
 
 import kr.co.daeun.notification.dto.CreateNotificationReqDTO;
+import kr.co.daeun.notification.dto.DeliveryAttemptDTO;
 import kr.co.daeun.notification.dto.NotificationDTO;
+import kr.co.daeun.notification.type.NotificationStatus;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface NotificationMapper {
     void insertNotification(NotificationDTO notificationDTO);
     NotificationDTO findByIdempotencyKey(String idempotencyKey);
+
+    NotificationDTO findNextProcessTarget();
+
+    int updateStatusToInProgress(Long notificationId, NotificationStatus currentStatus);
+
+    int updateNotificationSuccess(Long notificationId);
+
+    int updateNotificationRetry(NotificationDTO notificationDTO);
+
+    int updateNotificationDeadLetter(NotificationDTO notificationDTO);
+
+    void insertDeliveryAttempt(DeliveryAttemptDTO attemptDTO);
+
 }
