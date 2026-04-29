b385199cacc06f0ac31de3d556430d5d4fdc7479
daeunlee1125 <daeunlee1125@gmail.com>
2026-04-29 19:11:20 +0900

    email sender

---
+++ src/main/java/kr/co/daeun/notification/sender/EmailNotificationSender.java
@@ -0,0 +1,42 @@
+package kr.co.daeun.notification.sender;
+
+import kr.co.daeun.notification.dto.NotificationDTO;
+import kr.co.daeun.notification.type.ChannelType;
+import kr.co.daeun.notification.type.NotificationSendResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailNotificationSender implements NotificationSender {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Override
+    public ChannelType getChannelType() {
+        return ChannelType.EMAIL;
+    }
+
+    @Override
+    public NotificationSendResult send(NotificationDTO notification) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(fromEmail);
+            message.setTo(notification.getRecipientValue());
+            message.setSubject(notification.getTitle());
+            message.setText(notification.getBody());
+
+            mailSender.send(message);
+
+            return NotificationSendResult.success();
+        } catch (Exception e) {
+            return NotificationSendResult.fail("EMAIL_SEND_FAIL", e.getMessage());
+        }
+    }
+}
